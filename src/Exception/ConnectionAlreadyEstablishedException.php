94499f95951d223f20f686bf6519ff630a316849
Tomas Kliner <kliner.tomas@gmail.com>
2018-01-17 17:25:49 +0100

    Added own ftp and connection exceptions
    - FTPException - base own ftp exception
    - ConnectionException - general exception
    - ConnectionBadCredentialsException - exception for bad user credentials
    - ConnectionAlreadyEstablishedException - exception for already established connection

---
+++ src/Exception/ConnectionAlreadyEstablishedException.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the Speedy package.
+ *
+ * (c) Tomáš Kliner <kliner.tomas@gmail.com>
+ *
+ */
+
+namespace Speedy\FTP\Exception;
+
+/**
+ * Class ConnectionAlreadyEstablishedException
+ *
+ * @package     Speedy\FTP\Exception
+ * @author      Tomáš Kliner <kliner.tomas@gmail.com>
+ * @version     1.0.0
+ */
+class ConnectionAlreadyEstablishedException extends FTPException
+{
+
+}
